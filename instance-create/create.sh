d677ffaf6b357cee54b88c9da46ea87b20617956
zssurendradzelar <surendradudi432@gmail.com>
2021-07-26 13:00:32 +0530

    first

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -30,7 +30,7 @@ Instance_Create() {
 }
 
 if [ "$1" == "all" ]; then
-  for instance in frontend1 login1 todo1 redis1 users1 ; do
+  for instance in frontend login todo redis users ; do
     Instance_Create $instance-dev
   done
 else
